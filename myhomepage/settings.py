2082447b7b06371dff8c7c2f3ab847900b78aff6
鈴木貴大 <merioda.seven.24@gmail.com>
2020-11-21 22:33:03 +0900

    add code to settings.py

---
+++ myhomepage/settings.py
@@ -0,0 +1,56 @@
+from .settings_common import *
+import os
+
+DEBUG = False  # NOTE: 本番環境では必ずFalse
+
+# 許可するホスト名のリスト
+ALLOWED_HOSTS = [os.environ.get("ALLOWED_HOSTS")]
+
+# 静的ファイルを配置する場所
+STATIC_ROOT = "/usr/share/nginx/html/static"
+MEDIA_ROOT = "/usr/share/nginx/html/media"
+
+LOGGING = {
+    "version": 1,
+    "disable_existing_loggers": False,
+
+    "logger": {
+
+        # Djangoが利用するロガー
+        "django": {
+            "handlers": ["file"],
+            'level': 'INFO',  # INFO以上の深刻度のログだけを扱う
+        },
+
+        # homepageアプリケーションが利用するロガー
+        "homepage": {
+            "handlers": ["file"],
+            "level": "INFO",  # INFO以上の深刻度のログだけを扱う
+        },
+    },
+
+    # ハンドラの設定
+    "handlers": {
+        "file": {
+            "level": "INFO",
+            "class": "logging.handlers.TimedRotatingHandler",
+            "filename": os.path.join(BASE_DIR, "django.log"),
+            "formatter": "formatter1"
+            "when": "D",  # ログローテーションの間隔の単位を指定
+            "interval": 1,  # 何単位分でファイルを切り替えるか指定
+            "backupCount": 7,  # 保存しておくログファイル数を指定
+        }
+    }
+
+    # フォーマッタの設定
+    "formatters": {
+        "formatter1": {
+            "format": "\t".join([
+                "%(asctime)s",
+                "[%(levelname)s]",
+                "%(pathname)s(Line:%(lineno)d)",
+                "%(message)s"
+            ])
+        }
+    }
+}
